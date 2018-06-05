604089a8b01f4822d08b5c650fa0d620a0bcd5cc
PiotrDuz <piotrduzniak@outlook.com>
2018-06-05 22:52:18 +0200

    Added possibility to combine sensor measurements in one equation, with additional variables that user can set in GUI and save to XML.

--- SensorsReader/src/application/App.java
+++ SensorsReader/src/application/App.java
@@ -1,10 +1,22 @@
 package application;
 
 import javafx.application.Application;
+import operations.initializator.Xml;
+import operations.sensors.combination.SensorCombination;
+import operations.sensors.combination.SensorCombinationFactory;
 import userInterface.main.MainWindow;
 
 public class App {
 	public static void main(String[] args) {
+		// initialize classes saved in xml
+		Xml.retrieveXml();
+		// initialize custom sensor combinations
+		SensorCombinationFactory.initialize();
+
 		Application.launch(MainWindow.class, args);
+
+		for (SensorCombination sens : SensorCombinationFactory.combinationMap.values()) {
+			System.out.println(sens.getName() + "-    -" + sens.getVariables().toString());
+		}
 	}
 }
